d44686803131f0a2bb7391c69e39f08a689a70f6
gaby.santamaria@outlook.fr <gaby.santamaria@outlook.fr>
2021-04-01 00:38:32 +0200

    mercator projection

--- visualization/start.py
+++ visualization/start.py
@@ -7,17 +7,21 @@
 # visualization - visualize algorithms throught animations
 #--
 
+from manim import config
 from manimlib import *
 import numpy as np
 import xml.etree.ElementTree as xmlp
 
-def spheric_to_cartesian(lon, lat):
-    lon, lat = np.radians(lon), np.radians(lat)
-    return np.array(6371 * np.cos(lon) * np.cos(lat), 6371 * np.cos(lon) * np.sin(lat))
+# Using Mercator projection formula
+def mercator(lon, lat, width = 1920, height = 1080):
+    lon, lat, width, height = float(lon), float(lat), float(width), float(height)
+    r = width / (2 * PI)
+    x = r * np.radians(lon)
+    y = (height / 2) - r * np.log(np.tan(PI / 4 + np.radians(lat) / 2))
+    return np.array([x, y, 0])
 
 class Escape(Scene):
     def mapping(self):
-        width, height = config['frame_width'], config['frame_height']
         tree = xmlp.parse('map.osm')
         root = tree.getroot()
 
@@ -29,12 +33,25 @@ class Escape(Scene):
             t = child.tag
             att = child.attrib
             if t == 'bounds': bounds = att
-            if t == 'node': nodes[att['id']] = spheric_to_cartesian(att['lon'], att['lat'])
-            if t == 'way': ways[att['id']] = [nd.attrib['ref'] if nd.tag == 'nd' for nd in child]
+            if t == 'node':
+                bds = [mercator(bounds['minlon'], bounds['minlat']), mercator(bounds['maxlon'], bounds['maxlat'])]
+                xy = mercator(att['lon'], att['lat'])
+                nodes[att['id']] = np.array([xy[0], xy[1], 0])
+            if t == 'way': ways[att['id']] = [nd.attrib['ref'] for nd in child if nd.tag == 'nd']
+
+        lines = []
+        for way, nds in ways.items():
+            lines.append([Dot(nodes[node]) for node in nds])
+
+        return lines
 
     def population(self, n: int):
         return [Dot(color=random_color()) for k in range(n)]
 
     def construct(self):
-        population = self.population(40)
-        pass        
+        for l in self.mapping():
+            for n in l:
+                self.add(n)
+        for p in self.population(40):
+            self.add(p)
+        
