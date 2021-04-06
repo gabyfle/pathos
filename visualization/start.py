9acc951e83f21f988517554e9f560dc3a031608c
gaby.santamaria@outlook.fr <gaby.santamaria@outlook.fr>
2021-04-06 22:12:16 +0200

    dune init

--- visualization/start.py
+++ visualization/start.py
@@ -13,12 +13,13 @@ import numpy as np
 import xml.etree.ElementTree as xmlp
 
 # Using Mercator projection formula
-def mercator(lon, lat, width = 1920, height = 1080):
+def mercator(lon, lat, width = 14, height = 8):
     lon, lat, width, height = float(lon), float(lat), float(width), float(height)
+    s = 40075016 * np.cos(np.radians(lat)) / 2 ** 23 
     r = width / (2 * PI)
     x = r * np.radians(lon)
     y = (height / 2) - r * np.log(np.tan(PI / 4 + np.radians(lat) / 2))
-    return np.array([x, y, 0])
+    return np.array([x * s, y * s, 0])
 
 class Escape(Scene):
     def mapping(self):
@@ -35,12 +36,15 @@ class Escape(Scene):
             if t == 'bounds': bounds = att
             if t == 'node':
                 bds = [mercator(bounds['minlon'], bounds['minlat']), mercator(bounds['maxlon'], bounds['maxlat'])]
+                s = (bds[1][0] - bds[0][0], bds[1][1] - bds[0][1])
                 xy = mercator(att['lon'], att['lat'])
                 nodes[att['id']] = np.array([xy[0], xy[1], 0])
             if t == 'way': ways[att['id']] = [nd.attrib['ref'] for nd in child if nd.tag == 'nd']
 
         lines = []
         for way, nds in ways.items():
+            for node in nds:
+                print(nodes[node])
             lines.append([Dot(nodes[node]) for node in nds])
 
         return lines
