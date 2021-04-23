80ef039bbbf18c68665deaf50f560f32bc96aa77
Gabriel Santamaria <gaby.santamaria@outlook.fr>
2021-04-23 22:57:47 +0200

    adding tex files

--- pathos/lib/graph.ml
+++ pathos/lib/graph.ml
@@ -8,28 +8,28 @@
 (*                                                             *)
 (** * * * ** ** * * * * * * * ** * * * ** * * * * * * * * * * **)
 
-type node = { identifier: int; distance: int }
+type node = { key: int; distance: int }
 type graph = { edges: int array array; nodes: node array }
 
 let create (n: int) =
     let edges = Array.make_matrix n n 0 in
-    let nodes = Array.init n (fun k -> { identifier = k; distance = -1 }) in
+    let nodes = Array.init n (fun k -> { key = k; distance = -1 }) in
     { edges = edges; nodes = nodes }
 
 let size (g: graph) =
     Array.length g.nodes
 
 let add_edge (g: graph) (a: node) (b: node) (f: int) =
-    g.edges.(a.identifier).(b.identifier) <- f
+    g.edges.(a.key).(b.key) <- f
 
 let del_edge (g: graph) (a: node) (b: node) =
-    g.edges.(a.identifier).(b.identifier) <- 0
+    g.edges.(a.key).(b.key) <- 0
 
 let get_edge (g: graph) (a: node) (b: node) =
-    g.edges.(a.identifier).(b.identifier)
+    g.edges.(a.key).(b.key)
 
 let get_adj (g: graph) (nd: node) =
-    let ext = g.edges.(nd.identifier) in (* edges incident externally to v *)
+    let ext = g.edges.(nd.key) in (* edges incident externally to v *)
     let max = (Array.length ext) - 1 in (* nodes number *)
     let rec get_adj_aux (i: int) (acc: node list) =
         let l = if ext.(i) <> 0 then (g.nodes.(i) :: acc)
